4565f6ed0ce6b900892bf275e533d89ed1cc7be4
shishu1998 <shishu_1998@yahoo.com>
2016-02-10 18:13:14 -0500

    finished

--- script.js
+++ script.js
@@ -1,22 +1,30 @@
 var c = document.getElementById("Can");
 var ctx = c.getContext("2d");
-ctx.strokeStyle = "#ff0000";
 ctx.strokeRect(0,0,500,500);
+var xcor;
+var ycor;
 
-var drawcircle = function(e){
+
+var draw = function(e){
     ctx.beginPath();
-    var xcor = e.pageX;
-    var ycor = e.pageY;
-    ctx.arc(xcor,ycor,5,0,2*Math.PI);
+    ctx.arc(e.pageX,e.pageY,5,0,2*Math.PI);
     ctx.fillStyle = "#ff0000";
     ctx.fill();
     ctx.stroke();
+    ctx.moveTo(e.pageX,e.pageY);
+    ctx.lineTo(xcor,ycor);
+    ctx.stroke();
+    xcor = e.pageX;
+    ycor = e.pageY;
 }
 
-c.addEventListener("click",drawcircle);
+c.addEventListener("click",draw);
 
 var clear = function(e){
     ctx.clearRect(0,0,500,500);
+    ctx.strokeRect(0,0,500,500);
+    xcor = undefined;
+    ycor = undefined;
 }
 
 var button = document.getElementById("clear");
